5a847596b8a683c6c7d9457ddb5714eed2b0280c
lgants <lgants@gmail.com>
2016-11-29 18:06:45 -0600

    added time ago function

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,11 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
-  
+
   has_attached_file :image
   has_attached_file :image, styles: { :medium => "640x" }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   validates :user_id, presence: true
+  validates_length_of :caption, :minimum => 3, :maximum => 300, :allow_blank => false
 
 end
